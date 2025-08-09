9ec073520e7dfb18de001bf63f056c81d790152a
hshsjshsgsh <cicanoctavian@gmail.com>
2025-08-09 22:28:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import os
-from aiohttp import web
 
-async def handle(request):
-    return web.Response(text="Bot is running!")
-
-async def run_webserver():
-    app = web.Application()
-    app.add_routes([web.get("/", handle)])
-    runner = web.AppRunner(app)
-    await runner.setup()
-    port = int(os.environ.get("PORT", 8080))
-    site = web.TCPSite(runner, "0.0.0.0", port)
-    await site.start()
-    
 import discord
 from discord.ext import commands
 import os
@@ -714,11 +700,5 @@ async def cancel(ctx):
     tournament.__init__()
     await ctx.send("❌ Tournament cancelled.", delete_after=5)
 
+bot.run(TOKEN)
 
-import asyncio
-
-async def main():
-    await run_webserver()
-    await bot.start(os.getenv("TOKEN"))
-
-asyncio.run(main())
